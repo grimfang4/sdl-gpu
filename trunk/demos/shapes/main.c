dbaca3729d990c09970a86389727bad6559d61b9
grimfang4@gmail.com <grimfang4@gmail.com@0083c31c-79ef-93b9-589c-0d7b459da5d0>
2012-01-10 13:17:19 +0000

    Forgot shape init in demo.

--- trunk/demos/shapes/main.c
+++ trunk/demos/shapes/main.c
@@ -26,6 +26,8 @@ int main(int argc, char* argv[])
 	
 	printf("Using renderer: %s\n", GPU_GetCurrentRendererID());
 	
+	GPU_LoadShapeRenderer();
+	
 	Uint32 startTime = SDL_GetTicks();
 	long frameCount = 0;
 	
