3aaa9bab604045b60ea9ad46a645a2b966725593
grimfang4@gmail.com <grimfang4@gmail.com@0083c31c-79ef-93b9-589c-0d7b459da5d0>
2012-04-10 17:19:40 +0000

    Changed shapes to use floating point positions.

--- trunk/SDL_gpuShapes/SDL_gpuShapes.h
+++ trunk/SDL_gpuShapes/SDL_gpuShapes.h
@@ -18,27 +18,27 @@ typedef struct GPU_ShapeRenderer
 	float (*SetThickness)(struct GPU_ShapeRenderer* shapeRenderer, float thickness);
 	float (*GetThickness)(struct GPU_ShapeRenderer* shapeRenderer);
 	
-	void (*Pixel)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x, Sint16 y, SDL_Color color);
+	void (*Pixel)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x, float y, SDL_Color color);
 
-	void (*Line)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, SDL_Color color);
+	void (*Line)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x1, float y1, float x2, float y2, SDL_Color color);
 
-	void (*Arc)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x, Sint16 y, float radius, float startAngle, float endAngle, SDL_Color color);
+	void (*Arc)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x, float y, float radius, float startAngle, float endAngle, SDL_Color color);
 
-	void (*Circle)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x, Sint16 y, float radius, SDL_Color color);
+	void (*Circle)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x, float y, float radius, SDL_Color color);
 
-	void (*CircleFilled)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x, Sint16 y, float radius, SDL_Color color);
+	void (*CircleFilled)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x, float y, float radius, SDL_Color color);
 
-	void (*Tri)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Sint16 x3, Sint16 y3, SDL_Color color);
+	void (*Tri)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x1, float y1, float x2, float y2, float x3, float y3, SDL_Color color);
 
-	void (*TriFilled)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Sint16 x3, Sint16 y3, SDL_Color color);
+	void (*TriFilled)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x1, float y1, float x2, float y2, float x3, float y3, SDL_Color color);
 
-	void (*Rect)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, SDL_Color color);
+	void (*Rect)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x1, float y1, float x2, float y2, SDL_Color color);
 
-	void (*RectFilled)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, SDL_Color color);
+	void (*RectFilled)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x1, float y1, float x2, float y2, SDL_Color color);
 
-	void (*RectRound)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, float radius, SDL_Color color);
+	void (*RectRound)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x1, float y1, float x2, float y2, float radius, SDL_Color color);
 
-	void (*RectRoundFilled)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, float radius, SDL_Color color);
+	void (*RectRoundFilled)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, float x1, float y1, float x2, float y2, float radius, SDL_Color color);
 
 	void (*Polygon)(struct GPU_ShapeRenderer* shapeRenderer, GPU_Target* target, Uint16 n, float* vertices, SDL_Color color);
 
@@ -55,27 +55,27 @@ float GPU_SetThickness(float thickness);
 
 float GPU_GetThickness(void);
 
-void GPU_Pixel(GPU_Target* target, Sint16 x, Sint16 y, SDL_Color color);
+void GPU_Pixel(GPU_Target* target, float x, float y, SDL_Color color);
 
-void GPU_Line(GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, SDL_Color color);
+void GPU_Line(GPU_Target* target, float x1, float y1, float x2, float y2, SDL_Color color);
 
-void GPU_Arc(GPU_Target* target, Sint16 x, Sint16 y, float radius, float startAngle, float endAngle, SDL_Color color);
+void GPU_Arc(GPU_Target* target, float x, float y, float radius, float startAngle, float endAngle, SDL_Color color);
 
-void GPU_Circle(GPU_Target* target, Sint16 x, Sint16 y, float radius, SDL_Color color);
+void GPU_Circle(GPU_Target* target, float x, float y, float radius, SDL_Color color);
 
-void GPU_CircleFilled(GPU_Target* target, Sint16 x, Sint16 y, float radius, SDL_Color color);
+void GPU_CircleFilled(GPU_Target* target, float x, float y, float radius, SDL_Color color);
 
-void GPU_Tri(GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Sint16 x3, Sint16 y3, SDL_Color color);
+void GPU_Tri(GPU_Target* target, float x1, float y1, float x2, float y2, float x3, float y3, SDL_Color color);
 
-void GPU_TriFilled(GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Sint16 x3, Sint16 y3, SDL_Color color);
+void GPU_TriFilled(GPU_Target* target, float x1, float y1, float x2, float y2, float x3, float y3, SDL_Color color);
 
-void GPU_Rect(GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, SDL_Color color);
+void GPU_Rect(GPU_Target* target, float x1, float y1, float x2, float y2, SDL_Color color);
 
-void GPU_RectFilled(GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, SDL_Color color);
+void GPU_RectFilled(GPU_Target* target, float x1, float y1, float x2, float y2, SDL_Color color);
 
-void GPU_RectRound(GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, float radius, SDL_Color color);
+void GPU_RectRound(GPU_Target* target, float x1, float y1, float x2, float y2, float radius, SDL_Color color);
 
-void GPU_RectRoundFilled(GPU_Target* target, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, float radius, SDL_Color color);
+void GPU_RectRoundFilled(GPU_Target* target, float x1, float y1, float x2, float y2, float radius, SDL_Color color);
 
 void GPU_Polygon(GPU_Target* target, Uint16 n, float* vertices, SDL_Color color);
 
