b56c0278749112a2060d3baf6256a0792da2028e
grimfang4@gmail.com <grimfang4@users.noreply.github.com>
2012-12-17 20:22:32 +0000

    Added stb_image_tester for when SOIL needs to be out of the way.

--- SDL_gpu/OpenGL/SOIL/stb_image_aug.h
+++ SDL_gpu/OpenGL/SOIL/stb_image_aug.h
@@ -5,9 +5,7 @@
 #ifndef HEADER_STB_IMAGE_AUGMENTED
 #define HEADER_STB_IMAGE_AUGMENTED
 
-#define STBI_HEADER_FILE_ONLY
-#include "stb_image.c"
-
+#include "stb_image.h"
 #include "stb_image_write.h"
 
 #endif
