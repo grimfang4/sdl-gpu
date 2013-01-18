773b79a7db59d286bbf0b869bab3cef52379ac5c
grimfang4@gmail.com <grimfang4@gmail.com@0083c31c-79ef-93b9-589c-0d7b459da5d0>
2013-01-18 17:46:31 +0000

    Added new NPOT demo.

---
+++ trunk/demos/npot/main.c
@@ -0,0 +1,71 @@
+#include "SDL.h"
+#include "SDL_gpu.h"
+#include <math.h>
+
+void printRenderers(void)
+{
+	const char* renderers[GPU_GetNumRegisteredRenderers()];
+	GPU_GetRegisteredRendererList(renderers);
+	
+	printf("Available renderers:\n");
+	int i;
+	for(i = 0; i < GPU_GetNumRegisteredRenderers(); i++)
+	{
+		printf("%d) %s\n", i+1, renderers[i]);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	printRenderers();
+	
+	GPU_Target* screen = GPU_Init(NULL, 800, 600, 0);
+	if(screen == NULL)
+		return -1;
+	
+	printf("Using renderer: %s\n", GPU_GetCurrentRendererID());
+	
+	GPU_Image* image = GPU_LoadImage("data/npot1.png");
+	if(image == NULL)
+		return -1;
+	
+	
+	Uint32 startTime = SDL_GetTicks();
+	long frameCount = 0;
+	
+	Uint8 done = 0;
+	SDL_Event event;
+	while(!done)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+				done = 1;
+			else if(event.type == SDL_KEYDOWN)
+			{
+				if(event.key.keysym.sym == SDLK_ESCAPE)
+					done = 1;
+			}
+		}
+		
+		GPU_ClearRGBA(screen, 200, 200, 255, 255);
+		
+		GPU_Blit(image, NULL, screen, image->w/2, image->h/2);
+		GPU_BlitTransform(image, NULL, screen, screen->w/2, screen->h/2, 360*sin(SDL_GetTicks()/2000.0f), 2.5*sin(SDL_GetTicks()/1000.0f), 2.5*sin(SDL_GetTicks()/1200.0f));
+		
+		GPU_Flip();
+		
+		frameCount++;
+		if(frameCount%500 == 0)
+			printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	}
+	
+	printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	
+	GPU_FreeImage(image);
+	GPU_Quit();
+	
+	return 0;
+}
+
+
