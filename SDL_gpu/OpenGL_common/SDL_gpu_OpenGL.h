abbdfe859bcf55871ee279f4f91271f3aeb67f07
grimfang4@gmail.com <grimfang4@users.noreply.github.com>
2013-03-27 18:42:43 +0000

    Fixed OpenGLES includes and definitions that were missed.

--- SDL_gpu/OpenGL_common/SDL_gpu_OpenGL.h
+++ SDL_gpu/OpenGL_common/SDL_gpu_OpenGL.h
@@ -3,6 +3,32 @@
 
 #include "SDL_gpu.h"
 
+#ifdef SDL_GPU_USE_OPENGL
+    #include "glew.h"
+#elif defined(SDL_GPU_USE_OPENGLES)
+    #include "GLES/gl.h"
+    #include "GLES/glext.h"
+
+    #define glFrustum glFrustumf
+    #define glOrtho glOrthof
+    #define glGenerateMipmap glGenerateMipmapOES
+    #define glDeleteFramebuffers glDeleteFramebuffersOES
+    #define glGenFramebuffers glGenFramebuffersOES
+    #define glFramebufferTexture2D glFramebufferTexture2DOES
+    #define glCheckFramebufferStatus glCheckFramebufferStatusOES
+    #define glBindFramebuffer glBindFramebufferOES
+    #define glBindFramebufferEXT glBindFramebufferOES
+    #define GL_FRAMEBUFFER GL_FRAMEBUFFER_OES
+    #define GL_COLOR_ATTACHMENT0 GL_COLOR_ATTACHMENT0_OES
+    #define GL_FRAMEBUFFER_COMPLETE GL_FRAMEBUFFER_COMPLETE_OES
+
+    #define glBlendEquation glBlendEquationOES
+    #define glBlendFuncSeparate glBlendFuncSeparateOES
+
+    #define GL_FUNC_ADD GL_FUNC_ADD_OES
+    #define GL_FUNC_SUBTRACT GL_FUNC_SUBTRACT_OES
+    #define GL_FUNC_REVERSE_SUBTRACT GL_FUNC_REVERSE_SUBTRACT_OES
+#endif
 
 typedef struct RendererData_OpenGL
 {
