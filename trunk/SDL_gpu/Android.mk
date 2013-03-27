e4572ebf8cae2fa0716cfb141427966984f86d0b
grimfang4@gmail.com <grimfang4@gmail.com@0083c31c-79ef-93b9-589c-0d7b459da5d0>
2013-03-27 19:09:42 +0000

    Added sprite-stress-demo for testing future blit optimizations.

--- trunk/SDL_gpu/Android.mk
+++ trunk/SDL_gpu/Android.mk
@@ -4,31 +4,22 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := SDL2_gpu
 
-RENDERER_DIR := OpenGLES_1
-SOIL_DIR := externals/SOIL
-VASE_REND_DIR := externals/vase_rend
+SDL_GPU_DIR := ./
+RENDERER_DIR := $(SDL_GPU_DIR)/OpenGL_common
+STB_IMAGE_DIR := $(SDL_GPU_DIR)/externals/stb_image
 
-LOCAL_CFLAGS := -I$(LOCAL_PATH)/../SDL/include -I$(LOCAL_PATH)/$(RENDERER_DIR) -I$(LOCAL_PATH)/$(SOIL_DIR) -I$(LOCAL_PATH)/$(VASE_REND_DIR)
+LOCAL_CFLAGS := -I$(LOCAL_PATH)/../SDL/include -I$(LOCAL_PATH)/$(SDL_GPU_DIR) -I$(LOCAL_PATH)/$(RENDERER_DIR) -I$(LOCAL_PATH)/$(STB_IMAGE_DIR)
 
-
-BASE_FILES := $(patsubst $(LOCAL_PATH)/%, %, $(wildcard $(LOCAL_PATH)/*.c))
-RENDERER_FILES := $(patsubst $(LOCAL_PATH)/%, %, $(wildcard $(LOCAL_PATH)/$(RENDERER_DIR)/*.c))
-
-$(info Local: $(LOCAL_PATH))
-$(info Soil: $(SOIL_DIR))
-$(info Something: $(RENDERER_FILES))
-
-LOCAL_SRC_FILES := $(BASE_FILES) \
-				   $(RENDERER_DIR)/SDL_gpu_OpenGLES_1.c \
-				   $(RENDERER_DIR)/SDL_gpuShapes_OpenGLES_1.c \
-				   $(SOIL_DIR)/image_DXT.c \
-				   $(SOIL_DIR)/image_helper.c \
-				   $(SOIL_DIR)/SOIL.c \
-				   $(SOIL_DIR)/stb_image.c \
-				   $(SOIL_DIR)/stb_image_write.c
+LOCAL_SRC_FILES := $(SDL_GPU_DIR)/SDL_gpu.c \
+				   $(SDL_GPU_DIR)/SDL_gpu_Renderer.c \
+				   $(SDL_GPU_DIR)/SDL_gpuShapes.c \
+				   $(RENDERER_DIR)/SDL_gpu_OpenGL.c \
+				   $(RENDERER_DIR)/SDL_gpuShapes_OpenGL.c \
+				   $(STB_IMAGE_DIR)/stb_image.c \
+				   $(STB_IMAGE_DIR)/stb_image_write.c
 
 
-LOCAL_CFLAGS += -DSDL_GPU_USE_OPENGLES_1 -DSTBI_FAILURE_USERMSG
+LOCAL_CFLAGS += -DSDL_GPU_USE_OPENGLES -DSTBI_FAILURE_USERMSG -O3
 LOCAL_LDLIBS += -llog -lGLESv1_CM
 
 LOCAL_SHARED_LIBRARIES := SDL2
---
+++ trunk/demos/sprite-stress/main.c
@@ -0,0 +1,141 @@
+#include "SDL.h"
+#include "SDL_gpu.h"
+
+void printRenderers(void)
+{
+	const char* renderers[GPU_GetNumRegisteredRenderers()];
+	GPU_GetRegisteredRendererList(renderers);
+	
+	printf("Available renderers:\n");
+	int i;
+	for(i = 0; i < GPU_GetNumRegisteredRenderers(); i++)
+	{
+		printf("%d) %s\n", i+1, renderers[i]);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	printRenderers();
+	
+	GPU_Target* screen = GPU_Init(NULL, 800, 600, 0);
+	if(screen == NULL)
+		return -1;
+	
+	printf("Using renderer: %s\n", GPU_GetCurrentRendererID());
+	
+	GPU_Image* image = GPU_LoadImage("data/test3.png");
+	if(image == NULL)
+		return -1;
+	
+	
+	float dt = 0.010f;
+	
+	Uint32 startTime = SDL_GetTicks();
+	long frameCount = 0;
+	
+	int maxSprites = 50000;
+	int numSprites = 101;
+	
+	float* x = (float*)malloc(sizeof(float)*maxSprites);
+	float* y = (float*)malloc(sizeof(float)*maxSprites);
+	float* velx = (float*)malloc(sizeof(float)*maxSprites);
+	float* vely = (float*)malloc(sizeof(float)*maxSprites);
+	int i;
+	for(i = 0; i < maxSprites; i++)
+	{
+		x[i] = rand()%screen->w;
+		y[i] = rand()%screen->h;
+		velx[i] = 10 + rand()%screen->w/10;
+		vely[i] = 10 + rand()%screen->h/10;
+	}
+	
+	
+	Uint8 done = 0;
+	SDL_Event event;
+	while(!done)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+				done = 1;
+			else if(event.type == SDL_KEYDOWN)
+			{
+				if(event.key.keysym.sym == SDLK_ESCAPE)
+					done = 1;
+				else if(event.key.keysym.sym == SDLK_EQUALS || event.key.keysym.sym == SDLK_PLUS)
+				{
+					if(numSprites < maxSprites)
+						numSprites += 100;
+                    GPU_LogError("Sprites: %d\n", numSprites);
+				}
+				else if(event.key.keysym.sym == SDLK_MINUS)
+				{
+					if(numSprites > 1)
+						numSprites -= 100;
+					if(numSprites < 1)
+                        numSprites = 1;
+                    GPU_LogError("Sprites: %d\n", numSprites);
+				}
+			}
+		}
+		
+		for(i = 0; i < numSprites; i++)
+		{
+			x[i] += velx[i]*dt;
+			y[i] += vely[i]*dt;
+			if(x[i] < 0)
+			{
+				x[i] = 0;
+				velx[i] = -velx[i];
+			}
+			else if(x[i]> screen->w)
+			{
+				x[i] = screen->w;
+				velx[i] = -velx[i];
+			}
+			
+			if(y[i] < 0)
+			{
+				y[i] = 0;
+				vely[i] = -vely[i];
+			}
+			else if(y[i]> screen->h)
+			{
+				y[i] = screen->h;
+				vely[i] = -vely[i];
+			}
+		}
+		
+		GPU_Clear(screen);
+		
+		for(i = 0; i < numSprites; i++)
+		{
+			GPU_Blit(image, NULL, screen, x[i], y[i]);
+		}
+		
+		GPU_Flip();
+		
+		frameCount++;
+		if(SDL_GetTicks() - startTime > 5000)
+        {
+			printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+			frameCount = 0;
+			startTime = SDL_GetTicks();
+        }
+	}
+	
+	printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	
+	free(x);
+	free(y);
+	free(velx);
+	free(vely);
+	
+	GPU_FreeImage(image);
+	GPU_Quit();
+	
+	return 0;
+}
+
+
