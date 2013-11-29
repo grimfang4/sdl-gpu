3efa900091c7546354b6857358cd77dbfa0cee87
grimfang4@gmail.com <grimfang4@users.noreply.github.com>
2013-11-29 20:37:52 +0000

    Added init demo.

---
+++ demos/init/main.c
@@ -0,0 +1,63 @@
+#include "SDL.h"
+#include "SDL_gpu.h"
+
+void printRenderers(void)
+{
+	const char* renderers[GPU_GetNumRegisteredRenderers()];
+	GPU_GetRegisteredRendererList(renderers);
+	
+	printf("Available renderers:\n");
+	int i;
+	for(i = 0; i < GPU_GetNumRegisteredRenderers(); i++)
+	{
+		printf("%d) %s\n", i+1, renderers[i]);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	printRenderers();
+	
+	GPU_Target* screen = GPU_Init(NULL, 800, 600, 0);
+	if(screen == NULL)
+		return -1;
+	
+	printf("Using renderer: %s\n", GPU_GetCurrentRendererID());
+	
+	float dt = 0.010f;
+	
+	Uint32 startTime = SDL_GetTicks();
+	long frameCount = 0;
+	
+	Uint8 done = 0;
+	SDL_Event event;
+	while(!done)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+				done = 1;
+			else if(event.type == SDL_KEYDOWN)
+			{
+				if(event.key.keysym.sym == SDLK_ESCAPE)
+					done = 1;
+			}
+		}
+		
+		GPU_Clear(screen);
+		
+		GPU_Flip();
+		
+		frameCount++;
+		if(frameCount%500 == 0)
+			printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	}
+	
+	printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	
+	GPU_Quit();
+	
+	return 0;
+}
+
+
