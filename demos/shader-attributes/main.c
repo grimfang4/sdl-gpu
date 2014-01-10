5797cd115a4db72823c2fc6853d0cfddd1c3b21e
grimfang4@gmail.com <grimfang4@users.noreply.github.com>
2014-01-10 14:38:17 +0000

    Added shader-attributes demo.  It will eventually test sending attributes to shaders, but for now it's a great cliprect demo.

---
+++ demos/shader-attributes/main.c
@@ -0,0 +1,253 @@
+#include "SDL.h"
+#include "SDL_gpu.h"
+#include "common.h"
+
+
+GPU_ShaderBlock load_shaders(Uint32* v, Uint32* f, Uint32* p)
+{
+    GPU_Renderer* renderer = GPU_GetCurrentRenderer();
+    const char* vertex_shader_file;
+    const char* fragment_shader_file;
+    if(renderer->tier < 3)
+    {
+        vertex_shader_file = "shader-attributes/test.vert";
+        fragment_shader_file = "shader-attributes/test.frag";
+    }
+    else
+    {
+        vertex_shader_file = "shader-attributes/test3.vert";
+        fragment_shader_file = "shader-attributes/test3.frag";
+    }
+    
+    *v = GPU_LoadShader(GPU_VERTEX_SHADER, vertex_shader_file);
+    
+    if(!*v)
+        GPU_LogError("Failed to load vertex shader: %s\n", GPU_GetShaderMessage());
+    
+    *f = GPU_LoadShader(GPU_FRAGMENT_SHADER, fragment_shader_file);
+    
+    if(!*f)
+        GPU_LogError("Failed to load fragment shader: %s\n", GPU_GetShaderMessage());
+    
+    *p = GPU_LinkShaders(*v, *f);
+    
+    if(!*p)
+    {
+        GPU_LogError("Failed to link shader program: %s\n", GPU_GetShaderMessage());
+        return;
+    }
+    
+    GPU_ShaderBlock block = GPU_LoadShaderBlock(*p, "gpu_Vertex", "gpu_TexCoord", "gpu_Color", "modelViewProjection");
+    GPU_ActivateShaderProgram(*p, &block);
+    
+    return block;
+}
+
+void free_shaders(Uint32 v, Uint32 f, Uint32 p)
+{
+    GPU_FreeShader(v);
+    GPU_FreeShader(f);
+    GPU_FreeShaderProgram(p);
+}
+
+	
+static const int grid_row_size = 10;
+static const float grid_offset_x = 200;
+static const float grid_offset_y = 30;
+static const float grid_cell_w = 60;
+static const float grid_cell_h = 60;
+
+void add_sprite(float* positions, float* colors, float* src_rects, int* num_sprites, SDL_Color color, GPU_Rect src_rect)
+{
+    int i = *num_sprites;
+    
+	positions[2*i] = grid_offset_x + (i%grid_row_size)*grid_cell_w;
+	positions[2*i+1] = grid_offset_y + (i/grid_row_size)*grid_cell_h;
+	
+	colors[4*i] = color.r;
+	colors[4*i+1] = color.g;
+	colors[4*i+2] = color.b;
+	#ifdef SDL_GPU_USE_SDL2
+	colors[4*i+3] = color.a;
+	#else
+	colors[4*i+3] = color.unused;
+	#endif
+	
+	src_rects[4*i] = src_rect.x;
+	src_rects[4*i+1] = src_rect.y;
+	src_rects[4*i+2] = src_rect.w;
+	src_rects[4*i+3] = src_rect.h;
+	
+	(*num_sprites)++;
+}
+
+int timeloc = -1;
+
+void set_shader(Uint32 program, GPU_ShaderBlock* block)
+{
+    GPU_ActivateShaderProgram(program, block);
+    
+    timeloc = GPU_GetUniformLocation(program, "time");
+}
+
+int main(int argc, char* argv[])
+{
+	printRenderers();
+	
+	GPU_Target* screen = GPU_Init(800, 600, 0);
+	if(screen == NULL)
+		return -1;
+	
+	printCurrentRenderer();
+	
+	GPU_Image* image = GPU_LoadImage("data/happy_50x50.bmp");
+	if(image == NULL)
+		return -1;
+	
+	Uint32 v, f, p;
+	GPU_ShaderBlock block = load_shaders(&v, &f, &p);
+	
+	Uint8 shader_index = 1;
+	set_shader(p, &block);
+	
+	float dt = 0.010f;
+	
+	Uint32 startTime = SDL_GetTicks();
+	long frameCount = 0;
+	
+	int maxSprites = 100;
+	int numSprites = 0;
+	
+	float positions[2*maxSprites];
+	float colors[4*maxSprites];
+	float src_rects[4*maxSprites];
+	int i;
+	SDL_Color color = {255, 255, 255, 255};
+	GPU_Rect src_rect = {0, 0, image->w, image->h};
+	
+	add_sprite(positions, colors, src_rects, &numSprites, color, src_rect);
+	
+	int mx, my;
+	Uint32 mouse_state;
+	
+	Uint8 done = 0;
+	SDL_Event event;
+	while(!done)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+				done = 1;
+			else if(event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                if(event.button.x <= 150 && event.button.y <= 150)
+                {
+                    if(event.button.button == SDL_BUTTON_LEFT)
+                    {
+                        float dx = event.button.x/3 - src_rect.x;
+                        float dy = event.button.y/3 - src_rect.y;
+                        src_rect.x = event.button.x/3;
+                        src_rect.y = event.button.y/3;
+                        src_rect.w -= dx;
+                        src_rect.h -= dy;
+                    }
+                    else if(event.button.button == SDL_BUTTON_RIGHT)
+                    {
+                        src_rect.w = event.button.x/3 - src_rect.x;
+                        src_rect.h = event.button.y/3 - src_rect.y;
+                    }
+                }
+            }
+			else if(event.type == SDL_KEYDOWN)
+			{
+				if(event.key.keysym.sym == SDLK_ESCAPE)
+					done = 1;
+				else if(event.key.keysym.sym == SDLK_EQUALS || event.key.keysym.sym == SDLK_PLUS)
+				{
+					if(numSprites < maxSprites)
+                        add_sprite(positions, colors, src_rects, &numSprites, color, src_rect);
+				}
+				else if(event.key.keysym.sym == SDLK_MINUS)
+				{
+					if(numSprites > 0)
+						numSprites--;
+				}
+				else if(event.key.keysym.sym == SDLK_SPACE)
+				{
+				    shader_index++;
+				    shader_index %= 2;
+				    if(shader_index == 0)
+                        set_shader(0, NULL);
+                    else if(shader_index == 1)
+                        set_shader(p, &block);
+				}
+			}
+		}
+		
+		mouse_state = SDL_GetMouseState(&mx, &my);
+		if(mouse_state & (SDL_BUTTON_LMASK | SDL_BUTTON_RMASK))
+        {
+            if(mx <= 150 && my <= 150)
+            {
+                if(mouse_state & SDL_BUTTON_LMASK)
+                {
+                    float dx = mx/3 - src_rect.x;
+                    float dy = my/3 - src_rect.y;
+                    src_rect.x = mx/3;
+                    src_rect.y = my/3;
+                    src_rect.w -= dx;
+                    src_rect.h -= dy;
+                }
+                else if(mouse_state & SDL_BUTTON_RMASK)
+                {
+                    src_rect.w = mx/3 - src_rect.x;
+                    src_rect.h = my/3 - src_rect.y;
+                }
+            }
+        }
+		
+        GPU_SetUniformf(timeloc, SDL_GetTicks()/1000.0f);
+        
+		GPU_Clear(screen);
+		
+		// TODO: Sprite batch
+		for(i = 0; i < numSprites; i++)
+		{
+		    GPU_Rect r = {src_rects[4*i], src_rects[4*i+1], src_rects[4*i+2], src_rects[4*i+3]};
+		    GPU_SetRGBA(image, colors[4*i], colors[4*i+1], colors[4*i+2], colors[4*i+3]);
+		    GPU_Blit(image, &r, screen, positions[2*i], positions[2*i+1]);
+		}
+		
+		set_shader(0, NULL);
+		
+		GPU_BlitScale(image, NULL, screen, 75, 75, 3.0f, 3.0f);
+		SDL_Color red = {255, 0, 0, 255};
+		SDL_Color green = {0, 255, 0, 255};
+		SDL_Color blue = {0, 0, 255, 255};
+		GPU_Rectangle(screen, 3*src_rect.x, 3*src_rect.y, 3*(src_rect.x + src_rect.w), 3*(src_rect.y + src_rect.h), red);
+		GPU_CircleFilled(screen, 3*src_rect.x, 3*src_rect.y, 4, blue);
+		GPU_CircleFilled(screen, 3*(src_rect.x + src_rect.w), 3*(src_rect.y + src_rect.h), 4, green);
+		
+		if(shader_index == 1)
+            set_shader(p, &block);
+		
+		
+		GPU_Flip(screen);
+		
+		frameCount++;
+		if(frameCount%500 == 0)
+			printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	}
+	
+	printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	
+	GPU_FreeImage(image);
+	
+	free_shaders(v, f, p);
+	
+	GPU_Quit();
+	
+	return 0;
+}
+
+
