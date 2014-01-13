2cc98fef92e1346edcdf1ee0b86254216a55a34f
grimfang4@gmail.com <grimfang4@gmail.com@0083c31c-79ef-93b9-589c-0d7b459da5d0>
2014-01-13 05:40:56 +0000

    Made some changes toward getting the GLES 2 renderer working on iPhone.  (Thanks once again, Vitaly!)

--- trunk/SDL_gpu/GLES_2/SDL_gpu_GLES_2.h
+++ trunk/SDL_gpu/GLES_2/SDL_gpu_GLES_2.h
@@ -7,8 +7,8 @@
 #if !defined(SDL_GPU_DISABLE_GLES) && !defined(SDL_GPU_DISABLE_GLES_2)
 
 #ifdef __IPHONEOS__
-    #include <OpenGLES/ES2/gl2.h>
-    #include <OpenGLES/ES2/gl2ext.h>
+    #include <OpenGLES/ES2/gl.h>
+    #include <OpenGLES/ES2/glext.h>
 #else
     #include "GLES2/gl2.h"
     #include "GLES2/gl2ext.h"
