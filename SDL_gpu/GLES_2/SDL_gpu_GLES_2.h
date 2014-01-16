382b358fbeeddc4274185bd79e0a7012e04b406d
grimfang4@gmail.com <grimfang4@users.noreply.github.com>
2014-01-16 16:09:32 +0000

    Moved texture dimensions and has_mipmap into base image storage.

--- SDL_gpu/GLES_2/SDL_gpu_GLES_2.h
+++ SDL_gpu/GLES_2/SDL_gpu_GLES_2.h
@@ -43,8 +43,6 @@ typedef struct ImageData_GLES_2
 {
 	Uint32 handle;
 	Uint32 format;
-	Uint8 has_mipmaps;
-	Uint32 tex_w, tex_h;  // For power-of-two support
 } ImageData_GLES_2;
 
 typedef struct TargetData_GLES_2
--- SDL_gpu/OpenGL_3/SDL_gpu_OpenGL_3.h
+++ SDL_gpu/OpenGL_3/SDL_gpu_OpenGL_3.h
@@ -51,8 +51,6 @@ typedef struct ImageData_OpenGL_3
 {
 	Uint32 handle;
 	Uint32 format;
-	Uint8 has_mipmaps;
-	Uint32 tex_w, tex_h;  // For power-of-two support
 } ImageData_OpenGL_3;
 
 typedef struct TargetData_OpenGL_3
