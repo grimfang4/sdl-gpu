bdfa467ee497ef802f852e44cbc34696c1844318
grimfang4@gmail.com <grimfang4@users.noreply.github.com>
2014-02-05 16:29:50 +0000

    Added pixel-perfect demo for help testing proper pixel alignment.

---
+++ demos/pixel-perfect/main.c
@@ -0,0 +1,129 @@
+#include "SDL.h"
+#include "SDL_gpu.h"
+#include <math.h>
+#include "common.h"
+#include "compat.h"
+
+
+int main(int argc, char* argv[])
+{
+	printRenderers();
+	
+	GPU_Target* screen = GPU_Init(800, 600, GPU_DEFAULT_INIT_FLAGS);
+	if(screen == NULL)
+		return -1;
+	
+	printCurrentRenderer();
+	
+	GPU_Image* image = GPU_LoadImage("data/pixel_perfect.png");
+	if(image == NULL)
+		return -1;
+	
+	int mode = 0;
+	int num_modes = 5;
+	float x = 0.0f;
+	float y = 0.0f;
+	SDL_Color color = {0, 255, 0, 255};
+	
+	Uint8* keystates = SDL_GetKeyboardState(NULL);
+	
+	Uint32 startTime = SDL_GetTicks();
+	long frameCount = 0;
+	
+	Uint8 done = 0;
+	SDL_Event event;
+	while(!done)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+				done = 1;
+			else if(event.type == SDL_KEYDOWN)
+			{
+				if(event.key.keysym.sym == SDLK_ESCAPE)
+					done = 1;
+				else if(event.key.keysym.sym == SDLK_RETURN)
+                {
+					mode++;
+					if(mode >= num_modes)
+                        mode = 0;
+                }
+				else if(event.key.keysym.sym == SDLK_r)
+                {
+					x = y = 0.0f;
+                }
+				else if(event.key.keysym.sym == SDLK_f)
+                {
+                    if(image->filter_mode == GPU_NEAREST)
+                        GPU_SetImageFilter(image, GPU_LINEAR);
+                    else
+                        GPU_SetImageFilter(image, GPU_NEAREST);
+                }
+				else if(event.key.keysym.sym == SDLK_UP)
+					y -= 1.0f;
+				else if(event.key.keysym.sym == SDLK_DOWN)
+					y += 1.0f;
+				else if(event.key.keysym.sym == SDLK_LEFT)
+					x -= 1.0f;
+				else if(event.key.keysym.sym == SDLK_RIGHT)
+					x += 1.0f;
+			}
+			else if(event.type == SDL_KEYUP)
+			{
+                GPU_LogError("x, y: (%.2f, %.2f)\n", x, y);
+			}
+		}
+		
+        if(keystates[KEY_w])
+            y -= 0.1f;
+        else if(keystates[KEY_s])
+            y += 0.1f;
+        if(keystates[KEY_a])
+            x -= 0.1f;
+        else if(keystates[KEY_d])
+            x += 0.1f;
+		
+		GPU_Clear(screen);
+		
+		if(mode == 0)
+        {
+            // Blitting
+            GPU_Blit(image, NULL, screen, x + image->w/2.0f, y + image->h/2.0f);
+        }
+        else if(mode == 1)
+        {
+            // Vertical line
+            GPU_Line(screen, x, y + 1, x, y + screen->h - 1, color);
+        }
+        else if(mode == 2)
+        {
+            // Horizontal line
+            GPU_Line(screen, x + 1, y, x + screen->w - 1, y, color);
+        }
+        else if(mode == 3)
+        {
+            // Bounding rect
+            GPU_Rectangle(screen, x, y, x + screen->w - 1, y + screen->h - 1, color);
+        }
+        else if(mode == 4)
+        {
+            // Filled bounding rect
+            GPU_RectangleFilled(screen, x, y, x + screen->w - 1, y + screen->h - 1, color);
+        }
+		
+		GPU_Flip(screen);
+		
+		frameCount++;
+		if(frameCount%500 == 0)
+			printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	}
+	
+	printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	
+	GPU_FreeImage(image);
+	GPU_Quit();
+	
+	return 0;
+}
+
+
