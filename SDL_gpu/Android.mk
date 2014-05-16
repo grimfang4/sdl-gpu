5bcc6c3682d744c3e1316ecc805eb484b6069b17
grimfang4@gmail.com <grimfang4@users.noreply.github.com>
2014-05-16 12:44:06 +0000

    Updated Android.mk.

--- SDL_gpu/Android.mk
+++ SDL_gpu/Android.mk
@@ -5,7 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := SDL2_gpu
 
 SDL_GPU_DIR := ./
-RENDERER_DIR := $(SDL_GPU_DIR)/OpenGL_common
 STB_IMAGE_DIR := $(SDL_GPU_DIR)/externals/stb_image
 
 LOCAL_CFLAGS := -I$(LOCAL_PATH)/../SDL/include -I$(LOCAL_PATH)/$(SDL_GPU_DIR) -I$(LOCAL_PATH)/$(RENDERER_DIR) -I$(LOCAL_PATH)/$(STB_IMAGE_DIR)
@@ -13,14 +12,16 @@ LOCAL_CFLAGS := -I$(LOCAL_PATH)/../SDL/include -I$(LOCAL_PATH)/$(SDL_GPU_DIR) -I
 LOCAL_SRC_FILES := $(SDL_GPU_DIR)/SDL_gpu.c \
 				   $(SDL_GPU_DIR)/SDL_gpu_Renderer.c \
 				   $(SDL_GPU_DIR)/SDL_gpuShapes.c \
-				   $(RENDERER_DIR)/SDL_gpu_OpenGL.c \
-				   $(RENDERER_DIR)/SDL_gpuShapes_OpenGL.c \
+				   $(SDL_GPU_DIR)/GL_common/SDL_gpu_GL_matrix.c \
+				   $(SDL_GPU_DIR)/GLES_1/SDL_gpu_GLES_1.c \
+				   $(SDL_GPU_DIR)/GLES_2/SDL_gpu_GLES_2.c \
 				   $(STB_IMAGE_DIR)/stb_image.c \
 				   $(STB_IMAGE_DIR)/stb_image_write.c
 
 
-LOCAL_CFLAGS += -DSDL_GPU_USE_OPENGLES -DSTBI_FAILURE_USERMSG -O3
-LOCAL_LDLIBS += -llog -lGLESv1_CM
+LOCAL_CFLAGS += -DSDL_GPU_DISABLE_OPENGL -DSTBI_FAILURE_USERMSG -O3
+#LOCAL_LDLIBS += -llog -lGLESv1_CM
+LOCAL_LDLIBS += -llog -lGLESv2 -lGLESv1_CM
 
 LOCAL_SHARED_LIBRARIES := SDL2
 
