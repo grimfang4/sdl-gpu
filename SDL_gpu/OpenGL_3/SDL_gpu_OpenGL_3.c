f7880348ab3f975cfd3b63126d8bf302ec793d02
grimfang4@gmail.com <grimfang4@users.noreply.github.com>
2014-10-29 13:50:02 +0000

    Implemented pipeline fallback for batch blitting.  Added internal define SDL_GPU_ASSUME_SHADERS so shaders are enabled for GL versions which have them standard, without relying on the extension checks.

--- SDL_gpu/OpenGL_3/SDL_gpu_OpenGL_3.c
+++ SDL_gpu/OpenGL_3/SDL_gpu_OpenGL_3.c
@@ -14,6 +14,7 @@ void GPU_FreeRenderer_OpenGL_3(GPU_Renderer* renderer) {}
 // Most of the code pulled in from here...
 #define SDL_GPU_USE_OPENGL
 #define SDL_GPU_USE_BUFFER_PIPELINE
+#define SDL_GPU_ASSUME_SHADERS
 #define SDL_GPU_GL_TIER 3
 #define SDL_GPU_GLSL_VERSION 130
 #define SDL_GPU_GL_MAJOR_VERSION 3
