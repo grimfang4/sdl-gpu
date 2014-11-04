1f99c91ec771519bbcb0db93f13ef2e42f8ec252
grimfang4@gmail.com <grimfang4@gmail.com@0083c31c-79ef-93b9-589c-0d7b459da5d0>
2014-11-04 04:21:15 +0000

    Removing documentation for 0.8.0.

--- documentation/html/search/all_68.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['h',['h',['../structGPU__Rect_a852c2287f82fc80eb7b1505051d3ad05.html#a852c2287f82fc80eb7b1505051d3ad05',1,'GPU_Rect::h()'],['../structGPU__Image_a8f164258fbc96f088bb36b28d2633329.html#a8f164258fbc96f088bb36b28d2633329',1,'GPU_Image::h()'],['../structGPU__Target_a4224c06521958365d4338456c99a27b3.html#a4224c06521958365d4338456c99a27b3',1,'GPU_Target::h()']]],
-  ['has_5fmipmaps',['has_mipmaps',['../structGPU__Image_a9cc9252d06238f65440843cd420ebac2.html#a9cc9252d06238f65440843cd420ebac2',1,'GPU_Image']]]
-];
--- documentation/html/search/variables_61.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['activateshaderprogram',['ActivateShaderProgram',['../structGPU__Renderer_a6dd8c883d0976e909a8cbbc92f3f77dd.html#a6dd8c883d0976e909a8cbbc92f3f77dd',1,'GPU_Renderer']]],
-  ['angle',['angle',['../structGPU__Camera_a33b5abf9dc01e50ffd8df2de88457fc0.html#a33b5abf9dc01e50ffd8df2de88457fc0',1,'GPU_Camera']]],
-  ['arc',['Arc',['../structGPU__Renderer_a0f719ce7b152557b69e36f50b239f765.html#a0f719ce7b152557b69e36f50b239f765',1,'GPU_Renderer']]],
-  ['arcfilled',['ArcFilled',['../structGPU__Renderer_a5aaf478aa8215817f8e62fb18904dda3.html#a5aaf478aa8215817f8e62fb18904dda3',1,'GPU_Renderer']]],
-  ['attachshader',['AttachShader',['../structGPU__Renderer_a54a322170dc3e3b9c5ae7e94fe73665b.html#a54a322170dc3e3b9c5ae7e94fe73665b',1,'GPU_Renderer']]],
-  ['attribute',['attribute',['../structGPU__AttributeSource_ac3290bc69ab18b7373b261e2265a93a0.html#ac3290bc69ab18b7373b261e2265a93a0',1,'GPU_AttributeSource']]]
-];
