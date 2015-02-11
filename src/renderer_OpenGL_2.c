748ea7ea6401c716914c40115a98542896573f2f
grimfang4@gmail.com <grimfang4@users.noreply.github.com>
2015-02-11 23:07:56 +0000

    Reorganized project layout.  Moved includes into include/ and source files into src/.  Renamed some source files.  Updated CMakeLists and Android.mk.

--- src/renderer_OpenGL_2.c
+++ src/renderer_OpenGL_2.c
@@ -1,28 +1,29 @@
-#include "SDL_gpu_OpenGL_3.h"
+#include "SDL_gpu_OpenGL_2.h"
 #include "SDL_gpu_RendererImpl.h"
 
 
-#if defined(SDL_GPU_DISABLE_OPENGL) || defined(SDL_GPU_DISABLE_OPENGL_3)
+#if defined(SDL_GPU_DISABLE_OPENGL) || defined(SDL_GPU_DISABLE_OPENGL_2)
 
 // Dummy implementations
-GPU_Renderer* GPU_CreateRenderer_OpenGL_3(GPU_RendererID request) {return NULL;}
-void GPU_FreeRenderer_OpenGL_3(GPU_Renderer* renderer) {}
+GPU_Renderer* GPU_CreateRenderer_OpenGL_2(GPU_RendererID request) {return NULL;}
+void GPU_FreeRenderer_OpenGL_2(GPU_Renderer* renderer) {}
 
 #else
-
 
 // Most of the code pulled in from here...
 #define SDL_GPU_USE_OPENGL
 #define SDL_GPU_USE_BUFFER_PIPELINE
 #define SDL_GPU_ASSUME_SHADERS
 #define SDL_GPU_GL_TIER 3
-#define SDL_GPU_GLSL_VERSION 130
-#define SDL_GPU_GL_MAJOR_VERSION 3
-#include "../GL_common/SDL_gpu_GL_common.inl"
-#include "../GL_common/SDL_gpuShapes_GL_common.inl"
+#define SDL_GPU_GLSL_VERSION 120
+#define SDL_GPU_GL_MAJOR_VERSION 2
+#define SDL_GPU_NO_VAO
+
+#include "renderer_GL_common.inl"
+#include "renderer_shapes_GL_common.inl"
 
 
-GPU_Renderer* GPU_CreateRenderer_OpenGL_3(GPU_RendererID request)
+GPU_Renderer* GPU_CreateRenderer_OpenGL_2(GPU_RendererID request)
 {
     GPU_Renderer* renderer = (GPU_Renderer*)malloc(sizeof(GPU_Renderer));
     if(renderer == NULL)
@@ -31,7 +32,7 @@ GPU_Renderer* GPU_CreateRenderer_OpenGL_3(GPU_RendererID request)
     memset(renderer, 0, sizeof(GPU_Renderer));
 
     renderer->id = request;
-    renderer->id.renderer = GPU_RENDERER_OPENGL_3;
+    renderer->id.renderer = GPU_RENDERER_OPENGL_2;
     renderer->shader_language = GPU_LANGUAGE_GLSL;
     renderer->shader_version = SDL_GPU_GLSL_VERSION;
     
@@ -44,7 +45,7 @@ GPU_Renderer* GPU_CreateRenderer_OpenGL_3(GPU_RendererID request)
     return renderer;
 }
 
-void GPU_FreeRenderer_OpenGL_3(GPU_Renderer* renderer)
+void GPU_FreeRenderer_OpenGL_2(GPU_Renderer* renderer)
 {
     if(renderer == NULL)
         return;
