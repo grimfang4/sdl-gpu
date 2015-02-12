7935d28956b650c53c7cc46217af595f01bf1026
grimfang4@gmail.com <grimfang4@gmail.com@0083c31c-79ef-93b9-589c-0d7b459da5d0>
2015-02-12 03:11:56 +0000

    Bumped to development version 0.11.0.

--- trunk/include/SDL_gpu.h
+++ trunk/include/SDL_gpu.h
@@ -14,7 +14,7 @@ extern "C" {
 
 // Compile-time versions
 #define SDL_GPU_VERSION_MAJOR 0
-#define SDL_GPU_VERSION_MINOR 10
+#define SDL_GPU_VERSION_MINOR 11
 #define SDL_GPU_VERSION_PATCH 0
 
 /* Auto-detect if we're using the SDL2 API by the headers available. */
