57e17cec6d0136ed358da1586fad542c42ec86aa
Jonathan Dearborn <grimfang4@gmail.com>
2016-07-30 18:02:43 -0400

    Added additional test for uncolored batches in triangle-batch-test.

--- tests/triangle-batch/main.c
+++ tests/triangle-batch/main.c
@@ -82,6 +82,73 @@ void fill_vertex_values(float* vertex_values, float* velx, float* vely, unsigned
     }
 }
 
+void fill_vertex_values_uncolored(float* vertex_values, float* velx, float* vely, unsigned int max_vertices, GPU_Target* screen, GPU_Image* image)
+{
+    unsigned int i;
+    int val_n = 0;
+    Uint16 w, h;
+    Uint16 tex_w, tex_h;
+    
+    if(image != NULL)
+    {
+        w = image->w;
+        h = image->h;
+        
+        tex_w = image->texture_w;
+        tex_h = image->texture_h;
+    }
+    else
+        w = h = tex_w = tex_h = 80;
+    
+    for(i = 0; i < max_vertices; i+=3)
+    {
+	    float offset_x1, offset_x2, offset_x3;
+	    float offset_y1, offset_y2, offset_y3;
+	    float x1, y1;
+	    
+		offset_x1 = rand()%(w/2);
+		offset_y1 = rand()%(h/2);
+		x1 = vertex_values[val_n++] = rand()%screen->w + offset_x1;
+		y1 = vertex_values[val_n++] = rand()%screen->h + offset_y1;
+        if(image != NULL)
+        {
+            vertex_values[val_n++] = offset_x1/tex_w;
+            vertex_values[val_n++] = offset_y1/tex_h;
+        }
+		
+		offset_x2 = 5 + rand()%(w/2);
+		offset_y2 = 5 + rand()%(h/2);
+		vertex_values[val_n++] = x1 + offset_x2;
+		vertex_values[val_n++] = y1 + offset_y2;
+        if(image != NULL)
+        {
+            vertex_values[val_n++] = (offset_x1 + offset_x2)/tex_w;
+            vertex_values[val_n++] = (offset_y1 + offset_y2)/tex_h;
+        }
+		
+		offset_x3 = -5 - rand()%(w/2);
+		offset_y3 = 5 + rand()%(h/2);
+		vertex_values[val_n++] = x1 + offset_x3;
+		vertex_values[val_n++] = y1 + offset_y3;
+        if(image != NULL)
+        {
+            vertex_values[val_n++] = (offset_x1 + offset_x3)/tex_w;
+            vertex_values[val_n++] = (offset_y1 + offset_y3)/tex_h;
+        }
+        
+        // Once per triangle
+        {
+            int n = i/3;
+            velx[n] = 10 + rand()%screen->w/10;
+            vely[n] = 10 + rand()%screen->h/10;
+            if(rand()%2)
+                velx[n] = -velx[n];
+            if(rand()%2)
+                vely[n] = -vely[n];
+        }
+    }
+}
+
 int do_interleaved(GPU_Target* screen)
 {
     Uint32 startTime;
@@ -534,7 +601,7 @@ int do_untextured(GPU_Target* screen)
                 else if(event.key.keysym.sym == SDLK_SPACE)
                 {
                     done = 1;
-                    return_value = 1;
+                    return_value = 5;
                 }
                 else if(event.key.keysym.sym == SDLK_EQUALS || event.key.keysym.sym == SDLK_PLUS)
                 {
@@ -615,6 +682,135 @@ int do_untextured(GPU_Target* screen)
 	return return_value;
 }
 
+int do_uncolored(GPU_Target* screen)
+{
+    Uint32 startTime;
+    long frameCount;
+    Uint8 done;
+    SDL_Event event;
+	
+	int return_value = 0;
+	
+	float dt = 0.010f;
+	
+	int max_vertices = 60000;
+	int num_vertices = 303;
+	
+	int floats_per_vertex = 2 + 2;  // xy, st
+	float* vertex_values = (float*)malloc(sizeof(float)*max_vertices*floats_per_vertex);
+	float* velx = (float*)malloc(sizeof(float)*max_vertices/3);
+	float* vely = (float*)malloc(sizeof(float)*max_vertices/3);
+	unsigned short* indices = (unsigned short*)malloc(sizeof(unsigned short)*max_vertices);
+	int i;
+    
+	GPU_Image* image = GPU_LoadImage("data/test3.png");
+	GPU_LogError("do_uncolored()\n");
+	if(image == NULL)
+		return -1;
+	
+	startTime = SDL_GetTicks();
+	frameCount = 0;
+	
+	fill_vertex_values_uncolored(vertex_values, velx, vely, max_vertices, screen, image);
+	// Setup indices
+	for(i = 0; i < max_vertices; i++)
+        indices[i] = i;
+	
+	
+	done = 0;
+	while(!done)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+				done = 1;
+			else if(event.type == SDL_KEYDOWN)
+			{
+				if(event.key.keysym.sym == SDLK_ESCAPE)
+					done = 1;
+				else if(event.key.keysym.sym == SDLK_SPACE)
+                {
+					done = 1;
+					return_value = 1;
+                }
+				else if(event.key.keysym.sym == SDLK_EQUALS || event.key.keysym.sym == SDLK_PLUS)
+				{
+				    num_vertices += 300;
+					if(num_vertices > max_vertices)
+						num_vertices = max_vertices;
+                    GPU_LogError("Vertices: %d\n", num_vertices);
+                    frameCount = 0;
+                    startTime = SDL_GetTicks();
+				}
+				else if(event.key.keysym.sym == SDLK_MINUS)
+				{
+					if(num_vertices > 3)
+						num_vertices -= 300;
+					if(num_vertices < 3)
+                        num_vertices = 3;
+                    GPU_LogError("Vertices: %d\n", num_vertices);
+                    frameCount = 0;
+                    startTime = SDL_GetTicks();
+				}
+			}
+		}
+		
+		// FIXME: Can cause squishing when a vertex hits a wall...
+		for(i = 0; i < num_vertices; i++)
+		{
+		    int n = i/3;
+		    int val_n = floats_per_vertex*i;
+			vertex_values[val_n] += velx[n]*dt;
+			vertex_values[val_n+1] += vely[n]*dt;
+			if(vertex_values[val_n] < 0)
+			{
+				vertex_values[val_n] = 0;
+				velx[n] = -velx[n];
+			}
+			else if(vertex_values[val_n] > screen->w)
+			{
+				vertex_values[val_n] = screen->w;
+				velx[n] = -velx[n];
+			}
+			
+			if(vertex_values[val_n+1] < 0)
+			{
+				vertex_values[val_n+1] = 0;
+				vely[n] = -vely[n];
+			}
+			else if(vertex_values[val_n+1] > screen->h)
+			{
+				vertex_values[val_n+1] = screen->h;
+				vely[n] = -vely[n];
+			}
+		}
+		
+		GPU_Clear(screen);
+		
+        GPU_TriangleBatch(image, screen, num_vertices, vertex_values, num_vertices, indices, GPU_BATCH_XY_ST);
+		
+		GPU_Flip(screen);
+		
+		frameCount++;
+		if(SDL_GetTicks() - startTime > 5000)
+        {
+			printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+			frameCount = 0;
+			startTime = SDL_GetTicks();
+        }
+	}
+	
+	printf("Average FPS: %.2f\n", 1000.0f*frameCount/(SDL_GetTicks() - startTime));
+	
+	free(vertex_values);
+	free(velx);
+	free(vely);
+	
+	GPU_FreeImage(image);
+	
+	return return_value;
+}
+
 int main(int argc, char* argv[])
 {
 	GPU_Target* screen;
@@ -638,6 +834,8 @@ int main(int argc, char* argv[])
             i = do_attributes(screen);
         else if(i == 4)
             i = do_untextured(screen);
+        else if(i == 5)
+            i = do_uncolored(screen);
         else
             i = 0;
     }
